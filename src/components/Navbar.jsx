33a7c1404671881333b72963453d79c2c55db7ba
문효안 <hyoanmoon@munhyoacbookair.home>
2024-10-22 14:13:37 -0400

    modify resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -122,7 +122,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1gWEr7K3SFMIZ4M_WfSPv7HvKZq9DvUo5/view?usp=sharing'
+              href= "https://drive.google.com/file/d/1wC8Sf6_VGl-qCnEQRMphG_3Y4BAisxoy/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
